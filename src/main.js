2335e3042d648a4518e3256e54386d8d2207fef7
amir <amirtaki623@gmail.com>
2025-08-24 22:43:05 +0330

    part 24

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const img = document.getElementById('image')
 const menu = document.querySelector("#menu")
 document.addEventListener("contextmenu",(e) => {
+    menu.style.display = "block"
     e.preventDefault();
 
 })
